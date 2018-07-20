20cdd2dd9a40d6b3bba671143a51916b31135bce
Zoey Ikemoto <zmi384@gmail.com>
2018-07-20 15:28:56 -0700

    changed server address from 3000 to 3004

--- server/index.js
+++ server/index.js
@@ -7,4 +7,4 @@ app.use(express.static(__dirname + '/../client/src/data/assets'));
 app.use(express.static(__dirname + '/../client/theming/images'));
 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3004, () => console.log('Example app listening on port 3004!'))
