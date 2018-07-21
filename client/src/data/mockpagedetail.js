6251cae849d5ec282c70ce81c5b98f2acf8529d7
Zoey Ikemoto <zmi384@gmail.com>
2018-07-20 18:57:01 -0700

    more stubbing out page details

--- client/src/data/mockpagedetail.js
+++ client/src/data/mockpagedetail.js
@@ -1,4 +1,5 @@
 //const details = whatver
+<<<<<<< b6b9c01dac20bdd02632fe2e8ec01dca9609d3d1
 const details = {
       'id': 222989,
       'unitName': 'Beautiful Guest Suite for 2',
@@ -30,3 +31,29 @@ const details = {
 //module.exports = details;
 export {details as details};
 
+=======
+const singleListingDetails = {
+      id: 222989,
+      unitName: 'Beautiful Guest Suite for 2',
+      unitImage: 'https://a0.muscache.com/im/pictures/50616050/43df6979_original.jpg',
+      hostId: 3062517,
+      hostName: 'Debbie',
+      isSuprhost: false,
+      unitAddress: 'Seattle, United States',
+      neighbourhood_cleansed: 'Seattle',
+      city: 'Seattle',
+      state: 'WA',
+      country_code: 'US',
+      property_type: 'Condominium',
+      room_type: 'Entire Guest Suite',
+      beds: 1,
+      unitPrice: '$70.00',
+      priceModifier: 'per night',
+      numberOfReviews: 581,
+      reviewScoresRating: 5,
+      freeCancellation: true,
+      description_short: 'This lovely, modern room and ensuite bath is detached from the main house, so you can enjoy privacy and quiet. We are a 5 min drive from Ballard and a 15 min drive from downtown. Welcome to our quiet, safe neighborhood!'
+};
+//export details as listingDetails;
+export singleListingDetails as listingDetails;
+>>>>>>> more stubbing out page details
