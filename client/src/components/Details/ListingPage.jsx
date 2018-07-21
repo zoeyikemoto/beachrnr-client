05fd65fe3bd56c27cbaff0cfdf700b88d9ceb24c
Zoey Ikemoto <zmi384@gmail.com>
2018-07-21 12:30:30 -0700

    details now receives dynamic (mock) data

--- client/src/components/Details/ListingPage.jsx
+++ client/src/components/Details/ListingPage.jsx
@@ -6,6 +6,7 @@ import ListingDetails from './ListingDetails.jsx';
 import styled from 'styled-components';
 
 
+
 class ListingPage extends React.Component {
   constructor(props) {
     super(props);
@@ -31,32 +32,3 @@ class ListingPage extends React.Component {
 
 
 export default ListingPage;
-
-
-// module.exports = {
-//       id: 222989,
-//       unitName: 'Beautiful Guest Suite for 2',
-//       unitImage: 'https://a0.muscache.com/im/pictures/50616050/43df6979_original.jpg',
-//       hostId: 3062517,
-//       hostName: 'Debbie',
-//       isSuprhost: false,
-//       unitAddress: 'Seattle, United States',
-//       neighbourhood_cleansed: 'Seattle',
-//       city: 'Seattle',
-//       state: 'WA',
-//       country_code: 'US',
-//       property_type: 'Condominium',
-//       room_type: 'Entire Guest Suite',
-//       beds: 1,
-//       unitPrice: '$70.00',
-//       priceModifier: 'per night',
-//       numberOfReviews: 581,
-//       reviewScoresRating: 5,
-//       freeCancellation: true,
-//       description_short: 'This lovely, modern room and ensuite bath is detached from the main house, so you can enjoy privacy and quiet. We are a 5 min drive from Ballard and a 15 min drive from downtown. Welcome to our quiet, safe neighborhood!',
-//       //description_long: tbd
-//       //house_rules: tbd
-//       //cancellation_policy: tbd
-//       //about_neighborhood: tbd
-// };
-// //expor
