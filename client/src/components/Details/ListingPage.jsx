eca3804c835c1508c203fdb996da67e887de56f3
Zoey Ikemoto <zmi384@gmail.com>
2018-07-28 15:32:54 -0700

    it works

--- client/src/components/Details/ListingPage.jsx
+++ client/src/components/Details/ListingPage.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { details } from '../../data/mockpagedetail.js';
 import style from '../../styles/listingdetails.css';
+import { details } from '../../data/mockpagedetail.js';
 import ListingDetails from './ListingDetails.jsx';
 import styled from 'styled-components';
 
 const ListingPage = (props) => {
-  console.log("Listing ID in listingpage: " + props.listingId);
-  var currentListing = details.find(function(listing) {
-    return listing.id+"" === props.listingId;
-  });
-  if(currentListing) {
+
     return (
-      <ListingDetails {...props} data={currentListing} />
+      <ListingDetails {...props} data={props.currentListing} />
       )
-  } else {
-    return (
-      <p>Listing not found.</p>
-    )
-  }
+
 
 
 }
--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { details } from '../data/mockpagedetail.js';
 import Navbar from './Navbar/Navbar.jsx';
 import ListingPage from './Details/ListingPage.jsx';
 import Booking from './Booking/Booking.jsx';
@@ -11,18 +11,25 @@ const Div = styled.div`
 `;
 
 const MainLayout = (props) => {
+  let currentListing = details.find(function(listing) {
+    return listing.id+"" === props.match.params.id;
+  });
+  if(currentListing) {
   return (
     <div>
       <div>
         <Navbar />
       </div>
       <Div>
-        <ListingPage {...props} listingId = {props.match.params.id}/>
+        <ListingPage {...props} listingId = {props.match.params.id} currentListing = {currentListing}/>
         <Booking {...props} listingId = {props.match.params.id}/>
         <Review {...props} listingId = {props.match.params.id}/>
       </Div>
     </div>
   )
+} else {
+  return (<p>Listing not found.</p>);
+}
 }
 
 export default MainLayout;
