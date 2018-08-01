e553ebe6fb3079dd3a5fce59967ccab49f235f60
Joseph Gomez <jbgomez88@gmail.com>
2018-07-31 19:48:33 -0700

    (style) create responsive grid layout (#69)
    * Implement semantic iu grid layout for responsive content layout
    
    * Simplify viewport meta tag for consistent adherence to  media queries
    
    * Readd accidentally removed props from merge conflict resolution

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import { details } from '../data/mockpagedetail.js';
+import { Grid } from 'semantic-ui-react';
+
 import Navbar from './Navbar/Navbar.jsx';
 import ListingPage from './Details/ListingPage.jsx';
 import Booking from './Booking/Booking.jsx';
 import Review from './Review/Review.jsx';
 
 const Div = styled.div`
-  margin: 90px 1rem 0 1rem;
+  margin: 90px auto 0 auto;
+  max-width: 1080px;
+  padding: 0 1rem;
+`;
+
+const MobileBookingWrapper = styled.div`
+  @media (min-width: 1200px) {
+    display: none;
+  }
 `;
 
 const MainLayout = (props) => {
@@ -16,22 +26,29 @@ const MainLayout = (props) => {
   });
   if(currentListing) {
     return (
-      <div>
+      <React.Fragment>
         <div>
           <Navbar />
         </div>
         <Div>
-          <ListingPage {...props} listingId = {props.match.params.id} currentListing = {currentListing}/>
-          <Booking {...props} listingId = {props.match.params.id}/>
-          <Review {...props} listingId = {props.match.params.id}/>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column mobile={16} tablet={16} largeScreen={9} widescreen={9}>
+                <ListingPage {...props} listingId={props.match.params.id} currentListing={currentListing} />
+                <MobileBookingWrapper><Booking {...props} listingId={props.match.params.id} /></MobileBookingWrapper>
+                <Review {...props} listingId={props.match.params.id} />
+              </Grid.Column>
+              <Grid.Column largeScreen={7} only="large screen">
+                <Booking {...props} listingId={props.match.params.id} />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Div>
-      </div>
+      </React.Fragment>
     )
   } else {
     return (<p>Listing not found.</p>);
   }
-}
-}
-
+};
 
 export default MainLayout;
