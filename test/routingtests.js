736da3312a0be7490c89c8edb9109141e4b83c67
Zoey Ikemoto <30506962+zoeyikemoto@users.noreply.github.com>
2018-08-02 21:58:05 -0700

    integration testing: added routing test framework (#75)
    * stubbing out page details
    
    * more stubbing out page details
    
    * more stubbing out page details
    
    * added hardcoded details page
    
    * added hardcoded details page
    
    * details now receives dynamic (mock) data
    
    * details component is styled and profiled
    
    * deleted conflicting file
    
    * installed faker
    
    * added hardcoded details page
    
    * added hardcoded details page
    
    * details component is styled and profiled
    
    * deleted conflicting file
    
    * new canon package-lock
    
    * hot potato!
    
    broke the infinite package lock torment loop
    
    * stubbing out page details
    
    * added hardcoded details page
    
    * deleted conflicting file
    
    * installed faker
    
    * added hardcoded details page
    
    * added hardcoded details page
    
    * new canon package-lock
    
    * hot potato!
    
    broke the infinite package lock torment loop
    
    * updated page detail properties
    
    * Details component displays live(mock) data
    
    * passed id from url to main layout
    
    * routing id threads through to listing page
    
    * listing page now displays url id!
    
    * it works
    
    * consolidated listingdetails and listingpage, deleted listingdetails
    
    * renumbered listing ID according to our new system
    
    * style conformation
    
    * added host icon property
    
    * removed irrelevant review/booking properties
    
    might need a foreign key later.
    
    * generated mock listing data
    
    * fixed mainlayout
    
    * added listing detail helper function for inventory api call
    
    * set up router testing framework

---
+++ test/routingtests.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const server = require('../server/index');
+
+
+describe('inventory service', function() {
+  describe('#get()', function() {
+    it('should return 200 if an id match is found', function(done) {
+      chai.request(server)
+        .get('/rooms/2912000')
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('should return 404 if there is no id match', function(done) {
+      chai.request(server)
+        .get('/rooms/1337')
+        .end(function (err, res) {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+    it('should return the listing object with the matching id', function(done) {
+      chai.request(server)
+        .get('/rooms/2912000')
+        .end(function (err, res) {
+          expect(res.body.unitName).to.equal("Beautiful Guest Suite for 2");
+          done();
+        });
+    });
+  });
+});
